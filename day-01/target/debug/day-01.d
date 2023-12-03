1ef57e2e4011399299162ecd240328974701d3f8
Patrick <work.patrick.schuster@gmail.com>
2023-12-03 12:56:06 +0100

    Day 01 & 02

--- day-01/target/debug/day-01.d
+++
@@ -1,1 +0,0 @@
-/Users/patrick.schuster/Documents/Projects/Advent\ of\ Code/2023/day-01/target/debug/day-01: /Users/patrick.schuster/Documents/Projects/Advent\ of\ Code/2023/day-01/src/main.rs
--- day-01/target/debug/deps/day_01-90600c7f878b17d1.d
+++
@@ -1,5 +0,0 @@
-/Users/patrick.schuster/Documents/Projects/Advent of Code/2023/day-01/target/debug/deps/libday_01-90600c7f878b17d1.rmeta: src/main.rs
-
-/Users/patrick.schuster/Documents/Projects/Advent of Code/2023/day-01/target/debug/deps/day_01-90600c7f878b17d1.d: src/main.rs
-
-src/main.rs:
