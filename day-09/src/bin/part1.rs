9cd163d8fd78d2d3048e840e0a5cc873f5fe9a3c
Patrick <work.patrick.schuster@gmail.com>
2023-12-09 13:57:42 +0100

    Day 09

---
+++ day-09/src/bin/part1.rs
@@ -0,0 +1,40 @@
+use std::fs;
+
+fn main() {
+    let content = fs::read_to_string("in.txt")
+        .expect("file not found");
+
+    let mut sum: i32 = 0;
+    let lines = content.lines();
+    for line in lines {
+        let numbers: Vec<i32> = line.split_whitespace()
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect();
+
+        let result = step_down(&numbers);
+        sum += result;
+        println!("{} -> {}", line, result);
+    }
+
+    println!("Sum: {}", sum);
+}
+
+fn step_down(numbers: &Vec<i32>) -> i32 {
+    let mut seen = numbers[0];
+    for i in 1..numbers.len() {
+        if numbers[i] != seen {
+            seen = numbers[i];
+            break;
+        }
+    }
+
+    if seen == numbers[0] {
+        return seen;
+    }
+
+    let calc = numbers.windows(2)
+        .map(|w| w[1] - w[0])
+        .collect::<Vec<i32>>();
+
+    return step_down(&calc) + numbers[calc.len()];
+}
