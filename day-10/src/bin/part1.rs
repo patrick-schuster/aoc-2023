abeadd484ce59c5e0b0045303365be113bb1d94e
Patrick <work.patrick.schuster@gmail.com>
2023-12-10 19:00:09 +0100

    refactor: replace vectors

--- day-10/src/bin/part1.rs
+++ day-10/src/bin/part1.rs
@@ -7,50 +7,52 @@ fn main() {
     let len = content.lines()
         .next().unwrap().len();
 
-    let content = content.replace("\n", "");
-    let content = content.as_bytes();
-
+    let mut content = content.replace("\n", "");
+    let content = unsafe { content.as_bytes_mut() };
     let start = content.iter()
         .position(|c| *c == b'S')
         .expect("No starting point found");
 
+    // Replace the start point (its a special case).
+    content[start] = {
+        let left = matches!(content[start.checked_sub(1).unwrap_or(start)], b'-' | b'F' | b'L');
+        let top = matches!(content[start.checked_sub(len).unwrap_or(start)], b'|' | b'F' | b'7');
+        let right = matches!(content[start + 1], b'-' | b'J' | b'7');
+        let bottom = matches!(content[start + len], b'|' | b'J' | b'L');
+
+        if top && left { b'J' }
+        else if top && right { b'L' }
+        else if bottom && left { b'7' }
+        else if bottom && right { b'F' }
+        else if top && bottom { b'|' }
+        else { b'-' }
+    };
+
+    let mut sum: u16 = 0;
     let mut position = start;
     let mut previous = start;
-    let mut sum: u16 = 0;
-
     loop {
-        let surroundings = match content[position] {
-            b'|' => vec![position - len, position + len],
-            b'-' => vec![position - 1, position + 1],
-            b'L' => vec![position - len, position + 1],
-            b'J' => vec![position - len, position - 1],
-            b'7' => vec![position + len, position - 1],
-            b'F' => vec![position + len, position + 1],
-            b'S' => {
-                let possibilities = [
-                    (position.checked_sub(1).unwrap_or(position), &[b'-', b'F', b'L']),
-                    (position.checked_sub(len).unwrap_or(position), &[b'|', b'F', b'7']),
-                    (position + 1, &[b'-', b'J', b'7']),
-                    (position + len, &[b'|', b'J', b'L']),
-                ];
-
-                possibilities.iter()
-                    .filter(|(pos, chars)| chars.contains(&content[*pos]))
-                    .map(|(p, _)| *p)
-                    .collect()
-            },
-            _ => vec![],
+        // All possible matches for the current position.
+        let matches = match content[position] {
+            b'|' => (position - len, position + len),
+            b'-' => (position - 1, position + 1),
+            b'L' => (position - len, position + 1),
+            b'J' => (position - len, position - 1),
+            b'7' => (position + len, position - 1),
+            b'F' => (position + len, position + 1),
+            _ => (position, position),
         };
 
-        for n in surroundings {
-            if n != previous && n != position {
-                sum += 1;
-                previous = position;
-                position = n;
-                break;
-            }
-        }
+        // Make sure to go forward, not backwards.
+        let value = if matches.0 == previous || matches.0 == position {
+            matches.1
+        } else {
+            matches.0
+        };
 
+        sum += 1;
+        previous = position;
+        position = value;
         if position == start {
             break;
         }
